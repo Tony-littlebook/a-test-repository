35db4ee36000d788bcf5cbd54660bc43dd1a4415
Tony-littlebook <42344329+Tony-littlebook@users.noreply.github.com>
2019-03-16 14:59:34 +0800

    Update StaticSearchTable.h

--- StaticSearchTable.h
+++ StaticSearchTable.h
@@ -2,20 +2,25 @@
 #define STATICSEARCHTABLE_H_INCLUDED
 
 #include "SelemType.h"
-
 #define LIST_INIT_SIZE 5500
-public class SSTable{
+class SSTable{
     ElemType* elem;
-    public int length;
-    SSTable(){};
-    public bool InitSSTable(const char* file); 
-    public int Search_Seq(KeyType key);
-    public int Search_Bin(KeyType key);
-    ~SSTable();
+    int length;
+public:
+        SSTable(){
+        elem = NULL;
+        length = 0;
+    };
+    bool InitSSTable(const char* file);
+    bool TraverseSSTable();
+    int Search_Seq(KeyType key);
+    int Search_Bin(KeyType key);
+    ~SSTable(){
+        if(this->elem){
+            delete elem;
+            elem = NULL;
+        }
+    };
 };
 
-#endif // STATICSEARCHTABLE_H_INCLUDE
-
-
-
-#endif // SELEMTYPE_H_INCLUDED
+#endif // STATICSEARCHTABLE_H_INCLUDED
