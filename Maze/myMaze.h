98f8e3b7a2b4eb8a2ed85f7e5a3fc10884adf47d
Tony-littlebook <42344329+Tony-littlebook@users.noreply.github.com>
2019-05-19 00:21:19 +0800

    Update myMaze.h

--- Maze/myMaze.h
+++ Maze/myMaze.h
@@ -10,4 +10,7 @@ typedef struct{
 void InitMaze(MazeType* maze, int* a, int row,int col);
 int MazePath(MazeType* maze, PosType startPos, PosType endPos);
 void PrintMaze(MazeType maze);
+int Pass(MazeType maze, PosType curpos);
+void FootPrint(MazeType* maze, PosType curpos);
+void MarkPrint(MazeType* maze, PosType curpos);
 #endif // MYMAZE_H_INCLUDED
