84d6611e93a79bccf5cec3d85c4ec7f6e2179242
Tony-littlebook <42344329+Tony-littlebook@users.noreply.github.com>
2019-05-19 00:19:12 +0800

    c语言课设，迷宫通路（完工）

--- Maze/base.h
+++ Maze/base.h
@@ -1,9 +1,10 @@
 #ifndef BASE_H_INCLUDED
 #define BASE_H_INCLUDED
 typedef struct{
-    int r;
     int c;
+    int r;
 }PosType;
-
+int Same(PosType pos1, PosType pos2);
+PosType NextPos(PosType pos, int di);
 
 #endif // BASE_H_INCLUDED
